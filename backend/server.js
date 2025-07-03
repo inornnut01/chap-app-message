fe7ac765dea8de4cb28d46b477ec7967f726ba36
inornnut01 <inornnut01@gmail.com>
2025-07-03 12:16:44 +0700

    backend part - Auth, Message, User

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./utils/db.connect.js";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json()); // parse json body - request body
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
